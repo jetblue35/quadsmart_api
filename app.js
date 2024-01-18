e7cdc3d52f12faafb4cfe9b43f24bcf05801804b
mamamia140 <m-h-k140@hotmail.com>
2024-01-18 15:36:46 +0300

    added certificates

--- app.js
+++ app.js
@@ -1,34 +1,31 @@
 require('dotenv').config();
 const express = require('express');
+const https = require('https'); // Import the 'https' module
+const fs = require('fs'); // Import the 'fs' module for reading SSL/TLS certificates
 const bodyParser = require('body-parser');
 const path = require('path');
 const cors = require('cors');
 var cookieParser = require('cookie-parser')
 var session = require('express-session')
-var cluster = require("cluster"); // Load Balancer
-var filter = require('content-filter') // reliable security for MongoDB applications against the injection attacks
+var cluster = require("cluster");
+var filter = require('content-filter')
 require("./v1/auth/passport");
 
-var https = require("https");
-
-// Using version 1
 const v1 = require("./v1/index.js");
 
-// Server port
 const port = 443;
 
 const RateLimit = require('express-rate-limit');
 const passport = require('passport');
 const apiLimiter = RateLimit({
-	windowMs: 1 * 60 * 1000, // 1 minutes
-	max: 10000, // Limit each IP to 10000 requests per `window` (here, per 1 minutes)
-	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
-	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
-})
+    windowMs: 1 * 60 * 1000,
+    max: 10000,
+    standardHeaders: true,
+    legacyHeaders: false,
+});
 
 const app = express();
 
-// apply rate limiter to all requests
 app.use(apiLimiter);
 
 app.disable('x-powered-by');
@@ -46,19 +43,28 @@ app.use(session({
     resave: false,
     saveUninitialized: true,
     cookie: { secure: false }
-  }));
-  
+}));
+
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use(bodyParser.json()); // for parsing application/json
-app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use(filter());
 
-app.use("/v1", v1); // Using the first version
+app.use("/v1", v1);
+
+// Read the SSL/TLS certificates
+const privateKey = fs.readFileSync("./server.key");
+const certificate = fs.readFileSync("./server.cert");
+
+const credentials = {
+    key: privateKey,
+    cert: certificate
+};
 
 if (process.env.API_CLUSTER) {
     if (cluster.isPrimary) {
@@ -66,19 +72,19 @@ if (process.env.API_CLUSTER) {
         var cpuCount = require('os').cpus().length;
         console.log(`Total CPU ${cpuCount}`);
 
-        // Create a worker for each CPU
         for (var worker = 0; worker < cpuCount; worker += 1) {
             cluster.fork();
         }
 
-        // Listen for dying workers
         cluster.on('exit', function () {
             cluster.fork();
         })
     } else {
-        app.listen(port, () => console.log(`Worker ID ${process.pid}, is running on http://localhost:` + port));
+        // Create an HTTPS server and use the Express app as a handler
+        const httpsServer = https.createServer(credentials, app);
+        httpsServer.listen(port, () => console.log(`Worker ID ${process.pid}, is running on https://localhost:` + port));
     }
 } else {
-    https.createServer(app).listen(port,  () => console.log(`Worker ID ${process.pid}, is running on https://localhost:` + port))
-    //app.listen(port, () => console.log(`Worker ID ${process.pid}, is running on http://localhost:` + port));
+    const httpsServer = https.createServer(credentials, app);
+    httpsServer.listen(port, () => console.log(`Worker ID ${process.pid}, is running on https://localhost:` + port));
 }
